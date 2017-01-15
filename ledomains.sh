2867b5465f7758d5f015ef4fdbb0f025c5a96987
Milan Pässler <me@petabyteboy.de>
2017-01-15 22:55:34 +0100

    automatization

---
+++ ledomains.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -n letsencrypt certonly >certonly.sh
+echo -n \ -d  map.eulenfunk.de\ >>certonly.sh  
+cat /opt/eulenfunk/map/sites|grep -v \#|grep instance |cut -d" " -f4|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh 
+cat /opt/eulenfunk/map/sites|grep -v \#|grep alias |cut -d" " -f2|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh
+cat /opt/eulenfunk/map/sites|grep -v \#|grep group |cut -d" " -f4|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh
+sed -i ':a;N;$!ba;s/\n/ /g' certonly.sh
