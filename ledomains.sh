42169b48f526d5cfe1410063a04ad8628eb887a6
Adorfer <adorferen@gmail.com>
2017-03-30 23:36:35 +0200

    nur zeilen mit "#" *am anfang* sind kommentare

--- ledomains.sh
+++ ledomains.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo -n letsencrypt certonly >certonly.sh
-cat /opt/eulenfunk/map/sites|grep -v \#|grep basedom |cut -d" " -f4|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh 
-cat /opt/eulenfunk/map/sites|grep -v \#|grep instance |cut -d" " -f4|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh 
-cat /opt/eulenfunk/map/sites|grep -v \#|grep alias |cut -d" " -f2|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh
-cat /opt/eulenfunk/map/sites|grep -v \#|grep group |cut -d" " -f4|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh
+echo -n letsencrypt certonly > certonly.sh
+cat /opt/eulenfunk/map/sites|grep -v '^#'|grep basedom |cut -d" " -f4|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh
+cat /opt/eulenfunk/map/sites|grep -v '^#'|grep instance |cut -d" " -f4|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh
+cat /opt/eulenfunk/map/sites|grep -v '^#'|grep group |cut -d" " -f4|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh
+cat /opt/eulenfunk/map/sites|grep -v '^#'|grep alias |cut -d" " -f2|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >> certonly.sh
 sed -i ':a;N;$!ba;s/\n/ /g' certonly.sh
