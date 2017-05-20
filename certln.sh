0feb3c4b80811442b7ab13722eaac58688301e99
Adorfer <adorferen@gmail.com>
2017-05-21 00:26:30 +0200

    modified settings lvr

--- certln.sh
+++ certln.sh
@@ -4,17 +4,17 @@ dest=ddorf.map.eulenfunk.de
  ln -s $base/$dest $base/map.eulenfunk.de
 
 
-for dom in $(cat /opt/eulenfunk/map/sites|grep -v \#|grep instance |cut -d" " -f4|sed ':a;N;$!ba;s/\n/ /g'); do 
+for dom in $(cat /opt/eulenfunk/map/sites|grep -v ^\#|grep instance |cut -d" " -f4|sed ':a;N;$!ba;s/\n/ /g'); do 
  echo ln -s $base/$dest $base/$dom
  ln -s $base/$dest $base/$dom
 done
 
-for dom in $(cat /opt/eulenfunk/map/sites|grep -v \#|grep alias |cut -d" " -f2|sed ':a;N;$!ba;s/\n/ /g'); do 
+for dom in $(cat /opt/eulenfunk/map/sites|grep -v ^\#|grep alias |cut -d" " -f2|sed ':a;N;$!ba;s/\n/ /g'); do 
  echo ln -s $base/$dest $base/$dom
  ln -s $base/$dest $base/$dom
 done
 
-for dom in $(cat /opt/eulenfunk/map/sites|grep -v \#|grep group |cut -d" " -f4|sed ':a;N;$!ba;s/\n/ /g'); do 
+for dom in $(cat /opt/eulenfunk/map/sites|grep -v ^\#|grep group |cut -d" " -f4|sed ':a;N;$!ba;s/\n/ /g'); do 
  echo ln -s $base/$dest $base/$dom
   ln -s $base/$dest $base/$dom
 done
