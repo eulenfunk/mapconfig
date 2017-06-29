fa0b4d1e8c67fd25577208962d38cc5a1610f2dc
Adorfer <adorferen@gmail.com>
2017-06-29 16:43:16 +0200

    more routers for lln, less gw for rdv, lgf is small, one groupdomain less

--- certln.sh
+++ certln.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+#   /etc/letsencrypt/live/map.eulenfunk.de-0001/fullchain.pem
 base=/etc/letsencrypt/live
 dest=ddorf.map.eulenfunk.de
+dest=map.eulenfunk.de-0001
  ln -s $base/$dest $base/map.eulenfunk.de
-
 
 for dom in $(cat /opt/eulenfunk/map/sites|grep -v ^\#|grep instance |cut -d" " -f4|sed ':a;N;$!ba;s/\n/ /g'); do 
  echo ln -s $base/$dest $base/$dom
- ln -s $base/$dest $base/$dom
+ ln -f -s $base/$dest $base/$dom
 done
 
 for dom in $(cat /opt/eulenfunk/map/sites|grep -v ^\#|grep alias |cut -d" " -f2|sed ':a;N;$!ba;s/\n/ /g'); do 
  echo ln -s $base/$dest $base/$dom
- ln -s $base/$dest $base/$dom
+ ln -f -s $base/$dest $base/$dom
 done
 
 for dom in $(cat /opt/eulenfunk/map/sites|grep -v ^\#|grep group |cut -d" " -f4|sed ':a;N;$!ba;s/\n/ /g'); do 
  echo ln -s $base/$dest $base/$dom
-  ln -s $base/$dest $base/$dom
+  ln -f -s $base/$dest $base/$dom
 done
 
