db583535f4ebb90940035752d4b415e435f83d09
Adorfer <adorferen@gmail.com>
2021-07-19 23:27:49 +0200

    lat/lon swap

--- ledomains.sh
+++ ledomains.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo -n letsencrypt certonly > certonly.sh
+#echo -n letsencrypt certonly > certonly.sh
+echo -n certbot --nginx > certonly.sh
 cat /opt/eulenfunk/map/sites|grep -v '^#'|grep basedom |cut -d" " -f4|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh
 cat /opt/eulenfunk/map/sites|grep -v '^#'|grep instance |cut -d" " -f4|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh
 cat /opt/eulenfunk/map/sites|grep -v '^#'|grep group |cut -d" " -f4|sed s/^/\ -d\ /|sed ':a;N;$!ba;s/\n/ /g' >>certonly.sh
