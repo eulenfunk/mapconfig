234b8ffdd1098fd906d5f8c1703c722d3fd147ea
Adorfer <adorferen@gmail.com>
2022-05-01 00:49:05 +0200

    adding other domaintypes

--- check.sh
+++ check.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
 function check_service {
-	printf "P $1 $1="
-	result=$(systemctl status --no-pager $1 | grep "Active:"| grep -o -e failed -e inactive -e active)
-	num=0
-	if [ "$result" == "active" ] ; then num=1; fi
-	printf "$num;0.1:2;0:2 "
-	echo status=$result
+        printf "P $1 $1="
+        result=$(systemctl status --no-pager $1 | grep "Active:"| grep -o -e failed -e inactive -e active)
+        num=0
+        if [ "$result" == "active" ] ; then num=1; fi
+        printf "$num;0.1:2;0:2 "
+        echo status=$result
 }
 
 function check_instance {
-	check_service "fastd@$3"
-	check_service "hopglass-server@$3"
+        check_service "fastd@$3"
+        check_service "hopglass-server@$3"
+}
+
+function check_l2tpinstance {
+        check_service "tunneldigger@$3"
+        check_service "hopglass-server@$3"
+}
+
+function check_wgvxlaninstance {
+        check_service "tunneldigger@$3"
+        check_service "hopglass-server@$3"
 }
 
 cd $(dirname $0)
@@ -19,8 +29,10 @@ HOME=$PWD
 
 while read l
 do
-	#$l = TYPE NAME SITE URI PORT MTU
-	[ "$(echo $l | cut -d' ' -f1)" == "instance" ] && check_instance $l
+        #$l = TYPE NAME nef10wlf URI 8124 MTU
+        [ "$(echo $l | cut -d' ' -f1 )" == "instance" ] && check_instance $l
+        [ "$(echo $l | cut -d' ' -f1 )" == "instancel2tp" ] && check_l2tpinstance $l
+        [ "$(echo $l | cut -d' ' -f1 )" == "instancewgvxlan" ] && check_wgvxlaninstance $l
 done < $HOME/sites
 
 check_service nginx
